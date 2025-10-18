a34836ef46d29aba52c83de69ee65a73bba3d931
Erfan Sadigh Nejati <erfansn.es@gmail.com>
2025-10-18 17:13:51 +0330

    Batch similar entity in one draw call

--- app/src/main/java/ir/erfansn/rendering_playground/MainActivity.kt
+++ app/src/main/java/ir/erfansn/rendering_playground/MainActivity.kt
@@ -1,62 +1,35 @@
 package ir.erfansn.rendering_playground
 
-import android.graphics.Canvas
+import android.graphics.Color.BLACK
+import android.graphics.Path
 import android.graphics.PointF
-import android.graphics.Rect
 import android.graphics.RectF
+import android.graphics.RectF.intersects
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.collection.ObjectList
-import androidx.collection.mutableObjectListOf
-import androidx.compose.foundation.AndroidEmbeddedExternalSurface
 import androidx.compose.foundation.AndroidExternalSurface
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
-import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.lerp
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.util.fastForEach
 import androidx.core.graphics.withMatrix
-import androidx.core.os.ExecutorCompat
+import ir.erfansn.rendering_playground.element.ElementStyle
+import ir.erfansn.rendering_playground.element.InsertElement
 import ir.erfansn.rendering_playground.element.PointElement
 import ir.erfansn.rendering_playground.ui.theme.RenderingPlaygroundTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
-import kotlin.concurrent.thread
-import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
 
@@ -68,6 +41,7 @@ class MainActivity : ComponentActivity() {
                 val matrix = remember { android.graphics.Matrix() }
                 var redrawSignal by remember { mutableIntStateOf(0) }
                 val path = remember { android.graphics.Path() }
+                val path2 = remember { android.graphics.Path() }
                 val paint = remember { android.graphics.Paint() }
 
                 Box(
@@ -113,20 +87,50 @@ class MainActivity : ComponentActivity() {
                             }
 
                             surface.lockHardwareCanvas().apply {
-                                drawColor(Color.Black.toArgb())
+                                drawColor(BLACK)
                                 surface.unlockCanvasAndPost(this)
                             }
 
+                            var lastElementStyle: ElementStyle =
+                                SampleElements.first { it !is InsertElement }.style
                             val elementBounds = RectF()
                             snapshotFlow { redrawSignal }.collectLatest {
                                 surface.lockHardwareCanvas().apply {
-                                    drawColor(Color.Black.toArgb())
+                                    drawColor(BLACK)
                                     withMatrix(matrix) {
                                         SampleElements.fastForEach { element ->
                                             elementBounds.set(element.bounds)
                                             matrix.mapRect(elementBounds)
-                                            if (element is PointElement || ((elementBounds.width() > 4 || elementBounds.height() > 4) && RectF.intersects(viewBounds, elementBounds))) {
-                                                element.render(this, path, paint, matrix)
+
+                                            if (
+                                                element is PointElement || ((elementBounds.width() > 4 || elementBounds.height() > 4) &&
+                                                        intersects(viewBounds, elementBounds))
+                                            ) {
+                                                if (element is PointElement || element is InsertElement) {
+                                                    element.render(
+                                                        this,
+                                                        path2,
+                                                        paint,
+                                                        matrix
+                                                    )
+                                                } else if (element.style == lastElementStyle) {
+                                                    element.structure(path, matrix)
+                                                } else {
+                                                    val paint = paint.apply {
+                                                        this.color =
+                                                            lastElementStyle.color
+                                                        this.style =
+                                                            lastElementStyle.paintingStyle
+                                                    }
+
+                                                    if (!quickReject(path)) {
+                                                        drawPath(path, paint)
+                                                    }
+
+                                                    path.rewind()
+                                                    element.structure(path, matrix)
+                                                    lastElementStyle = element.style
+                                                }
                                             }
                                         }
                                     }
